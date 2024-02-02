d21b0a63098e79c4c77863cf0dfc40cde502baae
t3m14 <artemon.m174@gmail.com>
2024-02-02 11:39:02 +0500

    celery tasks added

--- app/config.py
+++ app/config.py
@@ -5,4 +5,4 @@ load_dotenv()
 
 # SQLALCHEMY_DATABSE_URI = os.environ.get("SQLALCHEMY_DATABSE_URI")
 
-SQLALCHEMY_DATABSE_URI =  "postgresql://postgres:postgres@localhost:1234/postgres"
+SQLALCHEMY_DATABSE_URI =  "postgresql://postgres:postgres@localhost:4321/postgres"
--- app/tasks/tasks.py
+++ app/tasks/tasks.py
@@ -1,3 +1,36 @@
+import cv2
+import base64
+import numpy as np
 from celery import Celery
 
-celery = Celery('tasks', broker='redis://redis:6379/0')
+app = Celery('tasks', broker='redis://localhost:6379/0')
+
+@app.task
+def apply_pipeline(image, pipeline_id):
+    # Преобразовать изображение в numpy-массив
+    np_array = np.frombuffer(image, np.uint8)
+    img = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+
+    # Изменить размер изображения до 640x640
+    img_resized = cv2.resize(img, (640, 640))
+
+    # Нормализовать изображение
+    img_normalized = cv2.normalize(img_resized, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+
+    # Преобразовать обработанное изображение в base64-строку
+    _, img_base64 = cv2.imencode('.jpg', img_normalized)
+    base64_str = base64.b64encode(img_base64.tobytes())
+
+    return base64_str
+
+@app.task
+def run_ml_model(processed_image):
+    # Имитация работы модели машинного обучения
+    # TODO: реализовать логику работы модели
+    return [100, 100, 200, 200, 0.9, 1]  # Пример координат бокса автомобиля
+
+@app.task
+def save_to_db(detected_box):
+    # Сохранение информации в БД
+    # TODO: реализовать сохранение в БД
+    pass
